e3d874de9b3775722cb7fd64a024059a19fd0166
Huy Truong <89716952+ttnhathuy313@users.noreply.github.com>
2024-11-20 15:21:31 +0700

    add cdn header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+		return [
+		  {
+			source: '/product/:path*',
+			headers: [
+			  {
+				key: 'Netlify-CDN-Cache-Control',
+				value: 'public, durable, max-age=86400, stale-while-revalidate=120',
+			  },
+			],
+		  },
+		];
+	  },
 };
 
 export default nextConfig;
